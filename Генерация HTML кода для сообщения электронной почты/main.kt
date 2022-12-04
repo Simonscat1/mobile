5a5176fa83daf3b3c3f732577829649c4eccc8f4
Simon <kazakeev_roman@mail.ru>
2022-12-04 15:00:18 +0800

    12

--- Генерация HTML кода для сообщения электронной почты/main.kt
+++ Генерация HTML кода для сообщения электронной почты/main.kt
@@ -1,51 +1,57 @@
 import java.io.PrintWriter
-data class Message(val sender:String?, val recipient:String?, val top:String?, val message:String?){
-    fun toHTML(): String {
-        val result_html = "<!DOCTYPE html>" +
-        	"<html>"+"<head>"+"<link rel='stylesheet' href='./style.css'>"+"</head>"+
-        	"<body>"+
-        	"<table>"+
-            if(sender){
-               sender?.let("<tr><td>Отправитель:</td><td>$it</td></tr>")
-            }
-            if(recipient){
-               recipient.let{"<tr><td>Получатель:</td><td>$it</td></tr>"}
-            }
-            if(top){
-               top.let{"<tr><td>Тема:</td><td>$it</td></tr>"}
-            }
-            if(message){
-               message.let{"\t\t\t<tr><td>Сообщение:</td><td>$it</td></tr>\n"}
-            }
-            "</table>"+
-            "</body>"
-        val writerHTML=PrintWriter("index.html")
-        writerHTML.append(result_html)
-        writerHTML.close()
-        return result_html
-	}
-    fun toStyle():String{
-          val result_style = "table{"+
-                    "border:2px solid black;"+
-                    "border-collapse:collapse;"+
-                    "font-size:24px;"+
-                    "}"+
-                    "td{"+
-                    "border:1px solid black;"+
-                    "}"+
-        val writerStyleHTML=PrintWriter("style.css")
-        writerStyleHTML.append(result_style)
-        writerStyleHTML.close()
-        return style
-    }
+
+data class Message(val sender:String?, val recipient:String?, val topic:String?, val message:String?){
+fun toHTML():String {
+     val  HTMLCode = "<!DOCTYPE html>"+
+          "<html>"+
+          "<head>"+
+          "<link rel=\"stylesheet\" href=\"style.css\">"+
+          "</head>"+
+          "<body>"+
+          "<table>"+
+          when(sender){
+               else->sender.let{"<tr><td class=\"nameRow\">Отправитель:</td><td>$it</td></tr>"}
+          }+
+          when(recipient){
+               else->recipient.let{"<tr><td class=\"nameRow\">Получатель:</td><td>$it</td></tr>"}
+          }+
+          when(topic){
+               else->topic.let{"<tr><td class=\"nameRow\">Тема:</td><td>$it</td></tr>"}
+          }+
+          when(message){
+               else->message.let{"<tr><td class=\"nameRow\">Сообщение:</td><td>$it</td></tr>"}
+          }+
+          "</table>"+
+          "</body>"+
+          "</html>"
+
+     val writerHTML=PrintWriter("main.html")
+     writerHTML.append(HTMLCode)
+     writerHTML.close()
+     return HTMLCode
 }
-
-
-
-
-
+fun toStyle():String{
+     val style=  "table{"+
+               "border:2px solid black;"+
+               "border-collapse:collapse;"+
+               "font-size:20px;"+
+               "}"+
+               "td{"+
+               "border:1px solid forestgreen;"+
+               "text-align:center;"+
+               "}"+
+               ".nameRow{"+
+               "text-align:left;"+
+               "background-color: #F7FF7F;"+
+               "}"
+     val writerStyleHTML=PrintWriter("style.css")
+     writerStyleHTML.append(style)
+     writerStyleHTML.close()
+     return style
+}
+}
 fun main(){
-    val mes=Message("plofjqfi1h1@gmail.com","dqwjqfj@mail.ru",null,"test")
-    println(mes.toHTML())
-    println(mes.toStyle())
+     val mes=Message("plofjqfi1h1@gmail.com","dqwjqfj@mail.ru",null,"test")
+     println(mes.toHTML())
+     println(mes.toStyle())
 }
